9b1503670c336911e0cff2b2afd8562dc3be0272
amrmoussa17 <amrmamdoh@hotmail.com>
2021-09-21 01:36:39 +0200

    add active class

--- js/app.js
+++ js/app.js
@@ -21,30 +21,25 @@
 const sections = document.querySelectorAll('section');
 const fragment = document.createDocumentFragment();
 const navBar = document.querySelector('#navbar__list');
+
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
+// get data-nav attribute value
 function getAttribute(element) {
     return element.getAttribute('data-nav');
 }
 
-// scroll to section if nav link is clicked 
-// advantage of Event delegation instead of instancing event listeners on every link  
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
 
-function scrollToSec(event) {
-    if (event.target.nodeName === 'A') {
-        const secName = event.target.innerText;
-        sections.forEach((section) => {
-            if (getAttribute(section) === secName) {
-                section.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-            }
-        }) 
-    }  
-}
-
+// build the nav function
 function buildNav() {
     sections.forEach((section) => {
         const navItemName = getAttribute(section);
@@ -54,23 +49,32 @@ function buildNav() {
         fragment.appendChild(navItem);
     })
     navBar.appendChild(fragment);
-    navBar.addEventListener('click',scrollToSec)
 };
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+// scroll to section when nav link is clicked function
+function scrollToSec(event) {
+    if (event.target.nodeName === 'A'){
+        const secName = event.target.innerText;
+        sections.forEach( (section) => {
+            if (getAttribute(section) === secName){
+                section.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+            }
+        }) 
+    }  
+}
 
-// build the nav
-document.addEventListener('DOMContentLoaded', buildNav);
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
+// Add active class function
+function addActive() {
+    sections.forEach( (section) => {
+        const top = section.getBoundingClientRect().top;
+        if (top >= 0 && top < 400){
+            section.classList.add('your-active-class')
+        }
+        else{
+            section.classList.remove('your-active-class')
+        }
+    })
+}
 
 /**
  * End Main Functions
@@ -79,9 +83,11 @@ document.addEventListener('DOMContentLoaded', buildNav);
 */
 
 // Build menu 
+document.addEventListener('DOMContentLoaded', buildNav);
 
 // Scroll to section on link click
+navBar.addEventListener('click',scrollToSec);
 
 // Set sections as active
-
+window.addEventListener('scroll',addActive);
 
