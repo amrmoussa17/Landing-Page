10e14a7716b9ca6868120e67a926990dc8510063
amrmoussa17 <amrmamdoh@hotmail.com>
2021-09-21 00:08:06 +0200

    add scroll to section fn

--- js/app.js
+++ js/app.js
@@ -20,14 +20,42 @@
 
 const sections = document.querySelectorAll('section');
 const fragment = document.createDocumentFragment();
-const navBar = document.querySelector('#navbar__list')
+const navBar = document.querySelector('#navbar__list');
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
+function getAttribute(element) {
+    return element.getAttribute('data-nav');
+}
 
+// scroll to section if nav link is clicked 
+// advantage of Event delegation instead of instancing event listeners on every link  
+
+function scrollToSec(event) {
+    if (event.target.nodeName === 'A') {
+        const secName = event.target.innerText;
+        sections.forEach((section) => {
+            if (getAttribute(section) === secName) {
+                section.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+            }
+        }) 
+    }  
+}
+
+function buildNav() {
+    sections.forEach((section) => {
+        const navItemName = getAttribute(section);
+        const navItem = document.createElement('li');
+        navItem.innerHTML = `<a> ${navItemName} </a>`;
+        navItem.firstElementChild.classList.add('menu__link');
+        fragment.appendChild(navItem);
+    })
+    navBar.appendChild(fragment);
+    navBar.addEventListener('click',scrollToSec)
+};
 
 /**
  * End Helper Functions
@@ -36,13 +64,7 @@ const navBar = document.querySelector('#navbar__list')
 */
 
 // build the nav
-sections.forEach((section) => {
-    const navItemName = section.getAttribute('data-nav');
-    const navItem = document.createElement('li');
-    navItem.innerText = navItemName;
-    fragment.appendChild(navItem);
-})
-navBar.appendChild(fragment);
+document.addEventListener('DOMContentLoaded', buildNav);
 
 // Add class 'active' to section when near top of viewport
 
